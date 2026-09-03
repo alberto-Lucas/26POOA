41f108c424de920d4e6aedf4c141c827e3b00bda
Lucas Alberto <lucas.alberto@linx.com.br>
2026-09-03 19:53:42 -0300

    VendaPOO

---
+++ VendaPOO/Produto.cs
@@ -0,0 +1,44 @@
+﻿namespace VendaPOO
+{
+    public class Produto
+    {
+        public string Descricao { get; set; }
+        public float PrecoUnitario { get; set; }
+        public int Quantidade { get; set; }
+        public float Desconto { get; set; }
+
+        //Propriedade que o total calculado do produto
+        //(quantidade * precoUnitario) - desconto
+        public float Total
+        {
+            get
+            {
+                return 
+                (Quantidade * PrecoUnitario) - Desconto;
+            }
+        }
+
+        //Propriedade para concatena as informações
+        //do produto
+        public string Informacao
+        {
+            get
+            {
+                //Ex: Descricao - 2 x 10,00 = 20,00 -
+                //Desconto: 5,00 Total: 15,00
+
+                //ToString() serve para converter
+                //numeros em texto
+                //ToString("0.00") serve para formatar
+                //o numero em formato moeda 2 -> 2,00
+                return
+                    Descricao + " - " +
+                    Quantidade.ToString() + " x " +
+                    PrecoUnitario.ToString("0.00") + " = " +
+                    (Quantidade * PrecoUnitario).ToString("0.00") + 
+                    " Desconto: R$ " + Desconto.ToString("0.00") + 
+                    " Total: R$ " + Total.ToString("0.00");
+            }
+        }
+    }
+}
---
+++ VendaPOO/Venda.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VendaPOO
+{
+    public class Venda
+    {
+        public DateTime DataHora { get; set; }
+        public string Vendedor { get; set; }
+        public string Cliente { get; set; }
+    }
+}
