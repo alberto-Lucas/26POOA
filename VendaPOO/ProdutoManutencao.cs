24aee1f7645e01a4537ed6197f6ecdba8239ede4
Lucas Alberto <lucas.alberto@linx.com.br>
2026-09-03 20:43:07 -0300

    VendaPOO

---
+++ VendaPOO/ProdutoManutencao.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace VendaPOO
+{
+    public class ProdutoManutencao
+    {
+        //Lista de Produtos
+        private List<Produto> listaProdutos =
+            new List<Produto>();
+
+        public void Adicionar(Produto objeto)
+        {
+            listaProdutos.Add(objeto);
+        }
+
+        public void Remover(Produto objeto)
+        {
+            listaProdutos.Remove(objeto);
+        }
+
+        public List<Produto> ListarProdutos()
+        {
+            return listaProdutos;
+        }
+
+        //Função para calcular o valor total
+        //da somatória de todos os produtos
+        public float TotalProdutos()
+        {
+            //Realizaremos um loop para 
+            //percorrer produtoa produtos da lista
+            //e somar os seus totais
+            float total = 0;
+
+            for (int i = 0; i < listaProdutos.Count; i++)
+                total += listaProdutos[i].Total;
+
+            return total;
+        }
+    }
+}
---
+++ VendaPOO/VendaManutencao.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace VendaPOO
+{
+    public class VendaManutencao
+    {
+        //Seguir o mesmo padrao de
+        //classe de manutencao
+        //1 - Lista de Objeto
+        //2 - Adicionar
+        //3 - Remover
+        //4 - Listar
+
+        private List<Venda> listaVendas =
+            new List<Venda>();
+
+        public void Adicionar(Venda objeto)
+        {
+            listaVendas.Add(objeto);
+        }
+
+        public void Remover(Venda objeto)
+        {
+            listaVendas.Remove(objeto);
+        }
+
+        public List<Venda> ListarVendas()
+        {
+            return listaVendas;
+        }
+    }
+}
