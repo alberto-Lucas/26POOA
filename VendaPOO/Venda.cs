f51dd74f8bc65eb1fa5cd2fcb2a03711f6b090ca
Lucas Alberto <lucas.alberto@linx.com.br>
2026-09-03 20:00:36 -0300

    VendaPOO

--- VendaPOO/Venda.cs
+++ VendaPOO/Venda.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace VendaPOO
 {
@@ -7,5 +8,14 @@ namespace VendaPOO
         public DateTime DataHora { get; set; }
         public string Vendedor { get; set; }
         public string Cliente { get; set; }
+
+        //A classe produto se torna o tipo de
+        //dado Produto e onde temos mais de um produto
+        //temos uma lista de produto, ou seja
+        //uma lista do tipo de dado produto
+        //neste caso comparando a banco de dados
+        //seria equivalente a FK da tabela
+        public List<Produto> Itens { get; set; }
+
     }
 }
